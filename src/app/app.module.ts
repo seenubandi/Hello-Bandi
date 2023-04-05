e578a53709fad6819ba6c1c58dc7a72800da35ca
BANDI SREENIVASULU <bandisreenivasulu@example.com>
2023-04-05 15:47:20 +0530

    Second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SeenuComponent } from './seenu/seenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/seenu/seenu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seenu',
+  templateUrl: './seenu.component.html',
+  styleUrls: ['./seenu.component.scss']
+})
+export class SeenuComponent {
+
+}
